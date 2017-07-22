00f615dafdaae8e43049b9ed6417153cb111d497
Joe Erickson <joe@techelevator.com>
2017-07-22 19:05:15 -0400

    Project initial seed

---
+++ database/schema.sql
@@ -0,0 +1,9 @@
+-- *************************************************************************************************
+-- This script creates all of the database objects (tables, sequences, etc) for the database
+-- *************************************************************************************************
+
+BEGIN;
+
+-- CREATE statements go here
+
+COMMIT;
