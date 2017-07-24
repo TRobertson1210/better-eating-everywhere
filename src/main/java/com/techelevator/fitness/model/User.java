a340336ff894245080b3c241b43522562caa521c
Tyler Robertson <trobertson1210@gmail.com>
2017-07-24 10:59:10 -0400

    User Model

--- src/main/java/com/techelevator/fitness/model/User.java
+++ src/main/java/com/techelevator/fitness/model/User.java
@@ -1,5 +1,83 @@
 package com.techelevator.fitness.model;
 
 public class User {
-
+	
+	private String email;
+	private String password;
+	private String confirmPassword;
+	private String name;
+	private int height;
+	private int weight;
+	private String sex;
+	private int targetWeight;
+	private double targetBMI;
+	private int phoneNumber;
+	
+	public User(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getTargetWeight() {
+		return targetWeight;
+	}
+	public void setTargetWeight(int targetWeight) {
+		this.targetWeight = targetWeight;
+	}
+	public double getTargetBMI() {
+		return targetBMI;
+	}
+	public void setTargetBMI(double targetBMI) {
+		this.targetBMI = targetBMI;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
 }
