8c2e61ba341051a14cb925806e65794f656b1d07
Jake Ravens <jakearavens@gmail.com>
2017-07-25 13:30:18 -0400

    changed return type of generateErrorMessage to a list of maps of strings

--- src/main/java/com/techelevator/fitness/validation/ValidationPojo.java
+++ src/main/java/com/techelevator/fitness/validation/ValidationPojo.java
@@ -1,10 +1,11 @@
 package com.techelevator.fitness.validation;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 
 public class ValidationPojo {
@@ -13,8 +14,22 @@ public class ValidationPojo {
 	private Map<String, List<FieldError>> errors;
 	
 	
-	public Map<String, List<FieldError>> generateErrorMessage(BindingResult result) {
-		errors.put("fieldName", result.getFieldErrors());
+	public List<Map<String, String>> generateErrorMessage(BindingResult result) {
+		List<FieldError> fieldErrorList = result.getFieldErrors();
+		
+		List<Map<String, String>> bigErrorList = new ArrayList<>();
+		
+		for(FieldError fieldError : fieldErrorList){
+			String defaultMessage = fieldError.getDefaultMessage();
+			String field = fieldError.getField();
+			Map<String, String> errorMap = new HashMap<>();
+			errorMap.put("Field", field);
+			errorMap.put("Message", defaultMessage);
+			bigErrorList.add(errorMap);
+		}
+				
+			
+		return bigErrorList;
 	}
 	
 }
